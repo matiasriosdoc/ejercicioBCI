de5ef249ce458c6e2855bc0bdef108bc2ed5314d
Matias Rios <matias.rios@docentes.unpaz.edu.ar>
2024-04-25 22:10:19 -0300

    Quito annotation inyeccion

--- src/main/java/com/globalogic/bci/ejercicioapi/EjercicioApiApplication.java
+++ src/main/java/com/globalogic/bci/ejercicioapi/EjercicioApiApplication.java
@@ -2,16 +2,8 @@ package com.globalogic.bci.ejercicioapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import com.globalogic.bci.ejercicioapi.EjercicioApiApplication;
 
 @SpringBootApplication
-@ComponentScan({"com.globallogic.bci.ejercicioapi"})
-@EntityScan("com.globallogic.bci.ejercicioapi")
-@EnableJpaRepositories("com.globallogic.bci.ejercicioapi")
 public class EjercicioApiApplication {
 
 	public static void main(String[] args) {
